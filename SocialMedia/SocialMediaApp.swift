68c21034c247ca9a74c5a07b39d0caab294119fa
mxnuchim <manuchimoliver779@gmail.com>
2023-03-09 14:17:33 +0100

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Manuchim Oliver on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
